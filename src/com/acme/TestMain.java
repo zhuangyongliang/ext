8a570824926a57953610fe4ed141cec65ed39f77
zhuangyongliang <yzhuang@aliyun.com>
2015-06-10 08:40:49 +0800

    modify format

--- src/com/acme/TestMain.java
+++ src/com/acme/TestMain.java
@@ -12,28 +12,28 @@ import wt.util.WTPropertyVetoException;
 public class TestMain {
 
     public static void main(String[] args) {
-       Pet myPet;
-    try {
-        myPet = Pet.newPet();
-        myPet.setName("Fergus");
-        myPet.setKind(PetKind.toPetKind("cat"));
-        DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
-        Timestamp ts = new Timestamp(df.parse("1999-12-30").getTime());
-        myPet.setDateOfBirth(ts);
-        myPet.setFixed(true);
-        PersistenceHelper.manager.save(myPet);
-        
-       
-    } catch (WTException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    } catch (WTPropertyVetoException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    } catch (ParseException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
+        Pet myPet;
+        try {
+            myPet = Pet.newPet();
+            myPet.setName("Fergus");
+            myPet.setKind(PetKind.toPetKind("cat"));
+            DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+            Timestamp ts = new Timestamp(df.parse("1999-12-30").getTime());
+            myPet.setDateOfBirth(ts);
+            myPet.setFixed(true);
+            PersistenceHelper.manager.save(myPet);
+            
+           
+        } catch (WTException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (WTPropertyVetoException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
       
        
     }
